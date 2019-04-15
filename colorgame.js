4b84c31284e8edc0bc641678f95ae0ab9dac28cd
unknown <xarodasimba@gmail.com>
2019-04-15 18:32:06 +0300

    added functions kai ligo stylizing

---
+++ colorgame.js
@@ -0,0 +1,64 @@
+var colors = generateRandomColors(6);
+var squares = document.querySelectorAll('.square');
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector("h1");
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+  //initial colos
+  squares[i].style.backgroundColor = colors[i]
+  //add click listeners to squares
+  squares[i].addEventListener('click', function() {
+    //grab color of clicked square
+
+    var clickedColor = this.style.backgroundColor;
+    //compare color to pickedColor
+    if (clickedColor === pickedColor) {
+      // alert("Correct");
+      messageDisplay.textContent = "Correct!"
+      changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
+    } else {
+      // alert("wrong");
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again"
+    }
+  });
+}
+
+//functions
+function changeColors(color) {
+  //loo through all squares
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(num) {
+  //make an array
+  var arr = [];
+  //repeat num times
+  for (var i = 0; i < num; i++) {
+    //get random color and push into array
+    arr.push(randomColor());
+  }
+  //return
+  return arr;
+}
+
+function randomColor() {
+	//pick a "red" from 0 - 255
+	var r = Math.floor(Math.random() * 256);
+	//pick a "green" from 0 - 255
+	var g = Math.floor(Math.random() * 256);
+	//pick a "blue" from 0 - 255
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
